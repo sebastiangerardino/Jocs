60ec5ae4b203606336ce0583d85001496ae566cc
sebastiangerardino <sege434@vidalibarraquer.net>
2026-01-29 10:56:43 +0100

    Add files via upload

---
+++ Jocs/src/main/java/org/yourcompany/yourproject/Jocs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author alumne
+ */
+public class Jocs {
+
+    public static void main(String[] args) {
+
+    }
+}
